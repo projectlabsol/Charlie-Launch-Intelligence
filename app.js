1b078afe8045a0597fcfb59fc9481284db735e8f
projectlabsol <charlie2522024@gmail.com>
2026-09-03 12:37:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ viralidad:92,
 seguridad:80
 },
 
+
 {
 nombre:"Animal Viral",
 ticker:"$ANV",
@@ -20,22 +21,26 @@ viralidad:85,
 seguridad:84
 }
 
+
 ];
+
 
 
 function analizarToken(token){
 
-let score = 
-(token.meta * 0.30) +
-(token.volumen * 0.25) +
-(token.comunidad * 0.20) +
-(token.viralidad * 0.15) +
-(token.seguridad * 0.10);
+let score =
+
+(token.meta*0.30)+
+(token.volumen*0.25)+
+(token.comunidad*0.20)+
+(token.viralidad*0.15)+
+(token.seguridad*0.10);
 
 
 return {
 
 ...token,
+
 score:Math.round(score)
 
 };
@@ -44,63 +49,42 @@ score:Math.round(score)
 
 
 
-function cargarDashboard(){
-
 let resultados = tokens.map(analizarToken);
+
 
 
 resultados.sort((a,b)=>b.score-a.score);
 
 
 
-document.getElementById("resultado").innerHTML = resultados.map(token=>`
+document.getElementById("resultado").innerHTML =
+
+resultados.map(token=>`
 
 <div class="card">
 
 <h3>${token.nombre}</h3>
 
-<p>${token.ticker}</p>
+<p class="badge">${token.ticker}</p>
 
-<div class="score">
+
+<p class="score">
 Score: ${token.score}/100
-</div>
+</p>
 
 
-<p class="label">Meta ${token.meta}</p>
-<div class="progress">
-<div style="width:${token.meta}%"></div>
-</div>
+<p>Meta: ${token.meta}</p>
 
+<p>Volumen: ${token.volumen}</p>
 
-<p class="label">Volumen ${token.volumen}</p>
-<div class="progress">
-<div style="width:${token.volumen}%"></div>
-</div>
+<p>Comunidad: ${token.comunidad}</p>
 
+<p>Viralidad: ${token.viralidad}</p>
 
-<p class="label">Comunidad ${token.comunidad}</p>
-<div class="progress">
-<div style="width:${token.comunidad}%"></div>
-</div>
-
-
-<p class="label">Viralidad ${token.viralidad}</p>
-<div class="progress">
-<div style="width:${token.viralidad}%"></div>
-</div>
-
-
-<p class="label">Seguridad ${token.seguridad}</p>
-<div class="progress">
-<div style="width:${token.seguridad}%"></div>
-</div>
+<p>Seguridad: ${token.seguridad}</p>
 
 
 </div>
+
 
 `).join("");
-
-}
-
-
-window.onload = cargarDashboard;
